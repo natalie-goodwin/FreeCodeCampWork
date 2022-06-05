b0e1f60d2f7946d7bd83e7789387a32273a80038
Natalie Goodwin <nmgoodwin27@gmail.com>
2022-06-04 22:44:40 -0500

    first commit

---
+++ code.js
@@ -0,0 +1,137 @@
+/* Comment your code 
+/* That's what I'm doing now */
+
+/*var number = 5; //in-line comment at the end of code
+
+/* multi-line 
+comment */
+
+// comments a single line
+
+/*let thisNumber = 9;
+console.log (thisNumber);*/
+
+//Data Types*/
+//undefined
+//null
+//boolean
+//string
+//symbol
+//number 
+//object
+
+//set data into a variable - label to point to the data
+
+/*var myName = "Natalie";
+let ourName = "freeCodeCamp"; //only within the scope where you declare into
+const pi = 3.14; // const never changes
+
+var a; //assign a variable
+var b = 2; //declare a variable; 2 is assigned to variable b
+b = a; //assigns value of a to b
+console.log(a); //logs value of variable in console
+
+var a = 9;  // var a declares variable ; 9 initializes variable
+
+var a = 5; 
+var b = 10; 
+var c = "I am a"; 
+
+a = a + 1; 
+b = b + 5; 
+c = c + " String !"; 
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+/* var sum = 10 + 0;
+console.log(sum);
+
+var difference = 45 - 0; 
+console.log(difference);
+
+var product = 8 * 10; 
+console.log(product);
+
+var quotient = 66/33;
+console.log(quotient); 
+
+myVar = myVar + 1; 
+myVar++; 
+
+myVar = myVar - 1;
+myVar --; 
+
+var ourDecimal = 5.7; //aka floating-point numbers
+
+var product = 2.5 * 2.5; 
+console.log(product);
+
+var quotient = 4.4 / 2.2; 
+console.log(quotient); 
+*/
+/*var remainder = 11 % 3;
+console.log(remainder); // finds remainder of 11 / 3
+//used to determine if number is even or odd
+
+// a = a + 12; /*is same as*/// a += 12;
+//b = 9 + b /*is same as*/ b += 9;
+
+// a = a - 6 /*is same as*/ a -= 6;
+//b = b - 6 /*is same as*/ b -= 6;
+
+//a = a * 5;/*is same as*/ a *= 5;
+//b = 3 * b; /*is same as*/ b *= 3
+
+//a = a / 12; /*is same as*/ a /= 12;
+//b = b / 4;  /*is same as*/ b /= 4;
+ 
+//var myFirstName = "Natalie";
+//var myLastName = "Goodwin";*/
+
+//back-slash quotation marks
+/*var myString = "I am a \"double-quoted\" string inside \"double quotes\"";
+console.log(myString);
+
+/* Callbacks: Javascript has first class functions - functions are treated 
+like any other variable and can be passed around. They can be passed as 
+parameters to other functions, and often are. Most array methods take a 
+function as a parameter and call that function as they do their job. 
+A function passed in a parameter to another function is called a callback 
+function.*/
+
+/*create an array with the names 'Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob'; 
+use a loop to iterate through the array and calculate the average number of 
+letters per name; print the result; Then use a loop to iterate again and 
+concatenate the names together, separated by spaces*/
+
+
+/*let sum = 0;
+let names = ['Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob']
+for (let i = 0; i < names.length; i++) {
+   sum += names[i].length;
+}
+
+console.log(sum/names.length)
+
+con = names[0];
+
+for (let i = 0; i < names.length; i++); {
+    con = con.concat(" ", names);
+}
+
+console.log(con);
+
+const threeArr = [1, 4, 6];
+const oneDown = threeArr.pop();
+//console.log(oneDown);
+//console.log(threeArr);*/
+
+let v = 12; 
+console.log(v);
+
+let g = 7; 
+
+console.log(g);
+console.log(v);
