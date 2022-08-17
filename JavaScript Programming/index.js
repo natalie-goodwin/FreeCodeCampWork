9bc26fe34a47678566eb379ea7a2523fd90cb5eb
Natalie Goodwin <nmgoodwin27@gmail.com>
2022-08-16 22:58:54 -0500

    new files August 16

---
+++ JavaScript Programming/index.js
@@ -0,0 +1,69 @@
+//document.getElementById("count-el").innerText= 5
+
+// let firstBatch = 5
+// let secondBatch = 7
+
+//let count = firstBatch + secondBatch
+
+
+//   let count = 5
+
+//   count = count + 2
+
+//    let bonusPoints = 50
+//    console.log(bonusPoints)
+
+//    bonusPoints = bonusPoints + 50
+
+// console.log(bonusPoints)
+// bonusPoints = bonusPoints - 75
+// console.log(bonusPoints)
+// bonusPoints = bonusPoints + 45
+// console.log (bonusPoints)
+
+
+//  let myAge = 41
+//  let humanDogRatio = 7
+ 
+//  myDogAge = myAge * humanDogRatio
+ 
+//  console.log(myDogAge)
+
+//Javascript recognizes if you reference a value before it has been initialized
+
+// create an Increment button with the id="increment-btn"
+ 
+
+// function increment() {
+//     console.log("The button was clicked")
+// }
+
+
+// function countdown() {
+//     console.log (5)
+//     console.log (4)
+//     console.log (3)
+//     console.log (2)
+//     console.log (1)
+// }
+// countdown()
+
+
+function awesomeJavaScript() {
+    console.log(42)
+}
+
+awesomeJavaScript()
+
+
+
+
+
+
+
+
+
+
+
+
+
